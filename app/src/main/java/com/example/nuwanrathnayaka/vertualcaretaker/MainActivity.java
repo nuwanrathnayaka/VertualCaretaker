a91e6f851e2ec466a9f80ad8ef093c3914591b28
nuwanrathnayaka <nuwanrathnayake.icc@gmail.com>
2017-05-08 15:12:15 +0530

    First commit

---
+++ app/src/main/java/com/example/nuwanrathnayaka/vertualcaretaker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.nuwanrathnayaka.vertualcaretaker;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
